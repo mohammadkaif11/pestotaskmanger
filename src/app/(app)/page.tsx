0c796f494d1b3dafe02974a454230b3ff647674f
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-03-10 17:51:05 +0530

    feat/get-task-backend_frontend

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,10 +1,34 @@
 import Link from "next/link";
+import CreateTaskModalButton from "~/components/Tasks/create/create-task-modal-button";
 import Navbar from "~/components/navbar/navbar";
+import { getServerAuthSession } from "~/server/auth";
+import { redirect } from "next/navigation";
+import { db } from "~/server/db";
+import TaskCard from "~/components/Tasks/TaskCard/task-card";
 
-export default function HomePage() {
+export default async function HomePage() {
+  const sessions = await getServerAuthSession();
+  if (!sessions?.user?.id) {
+    redirect("/login");
+  }
+  const tasks = await db.task.findMany({ where: { userId: sessions.user.id } });
   return (
     <main>
-      <Navbar/>
+      <Navbar />
+      <div className="container m-2">
+        <CreateTaskModalButton></CreateTaskModalButton>
+      </div>
+      <div className=" grid justify-items-center gap-4 sm:grid-cols-1 md:grid-cols-2  lg:grid-cols-3">
+        {tasks.map((task) => (
+          <TaskCard
+            id={task.id}
+            key={task.id}
+            title={task.title}
+            description={task.description}
+            status={task.status}
+          />
+        ))}
+      </div>
     </main>
   );
 }
---
+++ src/components/Tasks/TaskCard/task-card.tsx
@@ -0,0 +1,107 @@
+"use client";
+import React, { useState, useEffect } from "react";
+import { toast } from "sonner";
+import { TrashIcon, PencilSquareIcon } from "@heroicons/react/24/outline";
+import DeleteSceneModal from "../delete/delete-scene-modal";
+import EditSceneModal from "../update/update-scene-modal";
+
+interface ResponseType {
+  message: string;
+}
+function TaskCard({
+  id,
+  title,
+  description,
+  status,
+}: {
+  id: string;
+  title: string;
+  description: string;
+  status: number;
+}) {
+  const [statusId, setStatusId] = useState(status);
+  const [deleteModal, setDeleteModal] = useState(false);
+  const [updateModal, setUpdateModal] = useState(false);
+
+  useEffect(() => {
+    updateStatus().catch((err) => {
+      console.error(err);
+    });
+  }, [statusId]);
+
+  async function updateStatus() {
+    try {
+      const response = await fetch("/api/task-status-update", {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ id, statusId }),
+      });
+
+      if (!response.ok) {
+        throw new Error("Failed to Update Task");
+      }
+      const jsonResponse = (await response.json()) as ResponseType;
+      toast.success(jsonResponse.message);
+    } catch (error: unknown) {
+      const Error: ResponseType = {
+        message: (error as Error).message || "Internal Server Error",
+      };
+      toast.error(Error.message);
+      console.error("Error while updating Task Status:", error);
+    }
+  }
+
+  return (
+    <div className="max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow dark:border-gray-700 dark:bg-gray-800">
+      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+        {title}
+      </h5>
+      <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+        {description}
+      </p>
+      <select
+        id="countries"
+        defaultValue={statusId}
+        value={statusId}
+        onChange={(e) => {
+          setStatusId(Number(e.target.value));
+        }}
+        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+      >
+        <option value="1">Pending</option>
+        <option value="2">InProgress</option>
+        <option value="3">Done</option>
+      </select>
+      <div className="m-2 flex gap-2">
+        <TrashIcon
+          className="h-4 w-4 text-white"
+          onClick={() => {
+            setDeleteModal(true);
+          }}
+        />
+        <PencilSquareIcon
+          className="h-4 w-4 text-white"
+          onClick={() => {
+            setUpdateModal(true);
+          }}
+        />
+      </div>
+      <DeleteSceneModal
+        taskId={id}
+        open={deleteModal}
+        setOpen={setDeleteModal}
+      />
+      <EditSceneModal
+        taskId={id}
+        taskDescription={description}
+        taskTitle={title}
+        open={updateModal}
+        setOpen={setUpdateModal}
+      />
+    </div>
+  );
+}
+
+export default TaskCard;
