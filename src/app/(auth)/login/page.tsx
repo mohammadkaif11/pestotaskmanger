35994c9c6e63736dd7343bec3bf183ccd85acca6
mohammadkaif0211 <mohammadkaif@cofactory.ai>
2024-03-12 23:18:20 +0530

    change login page background color

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -4,11 +4,11 @@ import { Suspense } from "react";
 export default async function LoginPage() {
   return (
     <div className="mx-5 border border-black  py-10 sm:mx-auto sm:w-full sm:max-w-md sm:rounded-lg sm:shadow-md ">
-      <h4 className="text-2xl text-center font-bold text-emerald-500">PestoTaskManger</h4>
-      <h1 className="font-cal mt-6 text-center text-3xl text-emerald-400">
+      <h4 className="text-2xl text-center font-bold text-gray-500">PestoTaskManger</h4>
+      <h1 className="font-cal mt-6 text-center text-3xl text-gray-400">
         Welcome to Pesto TaskManger!
       </h1>
-      <p className="mb-8 mt-2 text-center text-sm text-emerald-400">
+      <p className="mb-8 mt-2 text-center text-sm text-gray-400">
         Mange daily task with Pesto task manger
       </p>
       <div className="mx-auto mt-4 w-11/12 max-w-xs sm:w-full">
